c25f6e2630063dab68fcd628b4b3e6103dbc0bfd
pdf114514 <haruton321@outlook.jp>
2024-01-19 12:36:56 +0900

    Commit

---
+++ Client/Localization.cs
@@ -0,0 +1,30 @@
+using System.Reflection;
+using System.Text.Json;
+
+namespace HiroshimaInfo.Client;
+
+public class Localization {
+    private readonly Dictionary<string, string> _Localizations = new();
+    public string Language { get; private set; } = "en";
+
+    public Localization(string language = "en") => SwitchLanguage(language);
+
+    public void SwitchLanguage(string language = "en") {
+        Language = language;
+        var assembly = Assembly.GetExecutingAssembly();
+        // <Assembly Name>.<Directory Name>.<File Name>
+        using var stream = assembly.GetManifestResourceStream($"{assembly.GetName().Name}.Localization.{language}.json") ?? throw new Exception($"Language {language} not found");
+        using var reader = new StreamReader(stream);
+        var json = reader.ReadToEnd();
+        var strings = JsonSerializer.Deserialize<Dictionary<string, string>>(json) ?? throw new Exception($"Language {language} not found");
+        _Localizations.Clear();
+        foreach (var (key, value) in strings) _Localizations.Add(key, value);
+    }
+
+    public string this[string key] => _Localizations.GetValueOrDefault(key, $"MISSING: {key}");
+
+    public static string[] SupportedLanguages { get {
+        var assembly = Assembly.GetExecutingAssembly();
+        return assembly.GetManifestResourceNames().Where(x => x.StartsWith($"{assembly.GetName().Name}.Localization") && x.EndsWith(".json")).Select(x => x.Split(".")[^2]).ToArray();
+    } }
+}
--- Client/Program.cs
+++ Client/Program.cs
@@ -1,11 +1,16 @@
 ﻿using HiroshimaInfo.Client;
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+using Microsoft.JSInterop;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+builder.Services.AddScoped(sp => new Localization(((IJSInProcessRuntime)sp.GetRequiredService<IJSRuntime>()).Invoke<bool>("eval", "/^ja\\b/.test(navigator.language)") ? "ja" : "en"));
+
+// Console.WriteLine(((IJSInProcessRuntime)builder.Services.First(x => x.ServiceType == typeof(IJSRuntime)).ImplementationInstance!).Invoke<bool>("eval", "/^ja\\b/.test(navigator.language)"));
+// foreach (var service in builder.Services) Console.WriteLine(service.ServiceType.FullName);
 
 await builder.Build().RunAsync();
